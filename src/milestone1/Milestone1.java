7c0bc2d232f4d8eee2d101b357f8f32643ffdb5f
Peter Nguyen <nguyenph88@gmail.com>
2013-09-23 21:47:49 -0700

    added Milestone1.java, Racer.java

--- src/milestone1/Milestone1.java
+++ src/milestone1/Milestone1.java
@@ -3,6 +3,15 @@
  */
 package milestone1;
 
+import lejos.nxt.LCD;
+import lejos.nxt.Motor;
+import lejos.nxt.Sound;
+import lejos.nxt.LightSensor;
+import lejos.nxt.SensorPort;
+import lejos.util.ButtonCounter;
+import lejos.robotics.navigation.DifferentialPilot;
+import robot.*;
+
 /**
  * @author Khoa Tran, Phuoc Nguyen
  *
@@ -11,8 +20,22 @@ public class Milestone1 {
 
 	/**
 	 * 
-	 */
+	 */	
+	
 	public static void main(String args[]) {
+		float wheelDiameter = 5.38f;
+		float trackWidth = 11.2f;
+		DifferentialPilot pilot = new DifferentialPilot(wheelDiameter,
+				trackWidth, Motor.A, Motor.C);
+		Scanner scanner = new Scanner(Motor.C, new LightSensor(SensorPort.S2));
+		Racer racer = new Racer(scanner, pilot);
+		Milestone1 milestone1 = new Milestone1();
+		
+		milestone1.go();
+	}
+	
+	public void go(){
+		
 	}
 
 }
