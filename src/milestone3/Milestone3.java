8e6b04ffb23832a5fd64c79368958adf8084e004
Khoa Tran <khoatran@berkeley.edu>
2013-10-04 15:22:50 -0700

    Milestone3 template

---
+++ src/milestone3/Milestone3.java
@@ -0,0 +1,40 @@
+package milestone3;
+
+import lejos.nxt.Button;
+import lejos.nxt.LCD;
+import lejos.nxt.Motor;
+import lejos.nxt.NXTRegulatedMotor;
+import lejos.nxt.Sound;
+import lejos.nxt.LightSensor;
+import lejos.nxt.UltrasonicSensor;
+import lejos.nxt.TouchSensor;
+import lejos.nxt.SensorPort;
+import lejos.robotics.navigation.DifferentialPilot;
+import robot.*;
+
+/**
+ * @author Khoa Tran, Phuoc Nguyen
+ */
+public class Milestone3 {
+
+	/**
+	 * Main code for Milestone 2
+	 * @param args - command line arguments
+	 */	
+	public static void main(String args[]) {
+		// Pilot-related variables
+		float wheelDiameter = 5.38f;
+		float trackWidth = 11.2f;
+		DifferentialPilot pilot = new DifferentialPilot(wheelDiameter,
+				trackWidth, Motor.A, Motor.C);
+		
+		// Sensor, Scanner, and Racer
+		NXTRegulatedMotor sensorMotor = Motor.B;
+		Scanner scanner = new Scanner(sensorMotor, new LightSensor(SensorPort.S2), new UltrasonicSensor(SensorPort.S3), new TouchSensor(SensorPort.S1), new TouchSensor(SensorPort.S4));
+		Racer racer = new Racer(scanner, pilot);
+		
+		// Wait for a press, and GO GO GO
+		Button.waitForAnyPress();
+		racer.findLight();
+	}
+}
